6451aa79a4de9338ec06234db26c60a8c33f170a
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-31 18:47:15 -0500

    Add a NavLink to return to the home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import { IoIosArrowBack } from 'react-icons/io';
 import { BsFillGearFill } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <header>
       <nav>
-        <IoIosArrowBack />
+        <NavLink to="/">
+          <IoIosArrowBack />
+        </NavLink>
         <input type="text" placeholder="City name..." />
         <BsFillGearFill />
       </nav>
