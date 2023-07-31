7337603b83e4e5c5f9447f86285426260cfc159b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-31 14:14:03 -0500

    Import styles module

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import australia from '../assets/australia.png';
 import europe from '../assets/europe.png';
 import nAmerica from '../assets/northern-america.png';
 import sAmerica from '../assets/south-america.png';
+import styles from '../styles/Home.module.css';
 
 const regions = {
   'Asia': asia,
@@ -16,11 +17,11 @@ const regions = {
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       {Object.keys(regions).map((name) => (
-        <div key={name}>
-          <img src={regions[name]} alt={name} />
-          <h2>{name}</h2>
+        <div key={name} className={styles.regions}>
+          <img src={regions[name]} alt={name} className={styles.images} />
+          <h2 className={styles.names}>{name}</h2>
         </div>
       ))}
     </div>
