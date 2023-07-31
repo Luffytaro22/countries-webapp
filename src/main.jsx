93ee496172a0a96b64718a5139e1c2d393ca993b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-31 14:39:41 -0500

    Dispatch the regions to the store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from '@reduxjs/toolkit'
 import App from './App.jsx'
 import store from './redux/store.js'
+import { getRegions } from './redux/home/regionSlice.js'
 import './index.css'
+
+store.dispatch(getRegions());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
