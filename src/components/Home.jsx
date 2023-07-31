3e0881aae2f333f43f2f3eb0aa57073d94a84b62
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-31 16:09:41 -0500

    Dispatch the getRegions function and render the regions state population

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,6 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { getRegions } from '../redux/home/regionSlice';
+import { useEffect } from 'react';
 import asia from '../assets/asia.png';
 import africa from '../assets/africa.png';
 import australia from '../assets/australia.png';
@@ -6,9 +9,9 @@ import nAmerica from '../assets/northern-america.png';
 import sAmerica from '../assets/south-america.png';
 import styles from '../styles/Home.module.css';
 
-const regions = {
+const regionsNames = {
   'Asia': asia,
-  'Australia': australia,
+  'Oceania': australia,
   'Europe': europe,
   'Africa': africa,
   'Northern America': nAmerica,
@@ -16,12 +19,23 @@ const regions = {
 }
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { regions } = useSelector((state) => state.regions);
+
+  useEffect(() => {
+    // Dispatch the getRegions action only once when the component mounts
+    Object.keys(regionsNames).forEach((name) => {
+      dispatch(getRegions(name));
+    });
+  }, []); // Empty array of dependencies ensures the useEffect runs only once on mount
+
   return (
     <div className={styles.container}>
-      {Object.keys(regions).map((name) => (
+      {Object.keys(regionsNames).map((name) => (
         <div key={name} className={styles.regions}>
-          <img src={regions[name]} alt={name} className={styles.images} />
+          <img src={regionsNames[name]} alt={name} className={styles.images} />
           <h2 className={styles.names}>{name}</h2>
+          <p>Population: {regions[name]}</p>
         </div>
       ))}
     </div>
