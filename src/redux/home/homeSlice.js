2f863d43ce07d55a6bb153d248b4317280bb67a4
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-31 14:25:42 -0500

    Created to fetch data from the Regions

---
+++ src/redux/home/homeSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API = 'https://api.apilayer.com/geo/country/region/';
+
+const config = {
+  headers: {
+    'apikey': 'ndkfTV5a2gUtSe9qUJ6I24UUypza2iR8',
+  }
+};
+
+const getRegions = createAsyncThunk('home/getRegions', async (region) => {
+  try {
+    const regionName = region.replace(' ', '%20');
+    const response = await axios.get(API + regionName, config);
+    return response.data;
+  } catch (error) {
+    throw new Error('Region not found');
+  }
+});
