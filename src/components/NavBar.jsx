a2464591c4b60af82ebf795537871c2b4533e8e9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-01 16:09:22 -0500

    Added a function to redirect the page when the user searchs for a country

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,34 @@
 import { IoIosArrowBack } from 'react-icons/io';
 import { BsFillGearFill } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const NavBar = () => {
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+  const findCountry = (event) => {
+    if (event.key === 'Enter') {
+      navigate('/detailsCountry', {
+        state: {
+          countryName: searchQuery,
+        }
+      })
+    }
+  };
   return (
     <header>
       <nav>
         <IoIosArrowBack onClick={() => navigate(-1)} />
-        <input type="text" placeholder="City name..." />
+        <input
+          type="text"
+          placeholder="Country name..."
+          onChange={handleInputChange}
+          onKeyDown={findCountry}
+          />
         <BsFillGearFill />
       </nav>
     </header>
