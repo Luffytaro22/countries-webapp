1de4e855cdc49e4e732214d0233c918f33904c29
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-01 09:44:09 -0500

    Add the new reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import regionReducer from "./home/regionSlice";
+import detailsRegionReducer from './details/detailsRegionSlice';
 
 const store = configureStore({
   reducer: {
     regions: regionReducer,
+    detailRegions: detailsRegionReducer,
   }
 });
 
