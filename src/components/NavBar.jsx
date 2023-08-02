4f8c1280e36ddcb6617728b8c2e39131411a8262
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-02 10:42:32 -0500

    Import the new styles component with classNames

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { BsFillGearFill } from 'react-icons/bs';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   const location = useLocation();
@@ -27,14 +28,15 @@ const NavBar = () => {
   };
 
   return (
-    <header>
-      <nav>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
         <IoIosArrowBack onClick={handleClick} />
         <input
           type="text"
           placeholder="Country name..."
           onChange={handleInputChange}
           onKeyDown={findCountry}
+          className={styles.input}
           />
         <BsFillGearFill />
       </nav>
