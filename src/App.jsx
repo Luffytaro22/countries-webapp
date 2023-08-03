1a397a9b611a8d86743fab632a43a99fcca19e55
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-03 11:19:25 -0500

    Add the Home components to the Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Regions from './components/Home';
+import Regions from './components/Regions';
 import DetailsRegion from './components/DetailsRegion';
 import DetailsCountry from './components/DetailsCountry';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route index path="/" element={<Regions />} />
+        <Route index path="/" element={<Home />} />
+        <Route path="/regions" element={<Regions />} />
         <Route path='/detailsRegion' element={<DetailsRegion />} />
         <Route path='/detailsCountry' element={<DetailsCountry />} />
       </Routes>
