177feff4b02b64245fe2dd45e710f02f7931958b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-08-06 10:15:38 -0500

    Add the calling to the api when the page loads

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 import store from './redux/store.js'
 import './index.css'
+import { getRegions } from './redux/home/regionSlice';
+
+const regionsNames = [
+  'Asia',
+  'Oceania',
+  'Europe',
+  'Africa',
+  'Northern America',
+  'South America',
+]
+
+regionsNames.forEach((name) => {
+  store.dispatch(getRegions(name));
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
